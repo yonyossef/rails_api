85a97aa8db2e03e18f4020890a6bf1c347f7b290
yonyossef <yonyossef3@gmail.com>
2021-11-20 23:46:09 +0200

    added employees controller

---
+++ app/controllers/api/v1/employees_controller.rb
@@ -0,0 +1,55 @@
+class Api::V1::EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :update, :destroy]
+
+  # GET /employees
+  def index
+    @employees = employee.all
+
+    render json: @employees.includes(:employees)
+  end
+
+  # GET /employees/1
+  def show
+    render json: @employee
+  end
+
+  # POST /employees
+  def create
+    @employee = employee.new(new_employee_params)
+
+    if @employee.save
+      render json: @employee, status: :created, location: @api_v1_employee
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /employees/1
+  def update
+    if @employee.update(employee_params)
+      render json: @employee
+    else
+      render json: @employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /employees/1
+  def destroy
+    @employee.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_employee
+      @employee = employee.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def employee_params
+      params.require(:employee).permit(:name)
+    end
+
+    def new_employee_params
+      params.permit(:name)
+    end
+end
